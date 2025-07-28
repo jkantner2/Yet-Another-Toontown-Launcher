af5c6bd07d58fa2a56fd9fc07dafc35a5a53ab45
autumnWindigo <autumn@windigo.xyz>
2025-07-28 19:58:48 -0400

    fix: refactored json for nice objects

--- lib/calculator/calculator.go
+++ lib/calculator/calculator.go
@@ -1,1 +1,17 @@
 package calculator
+
+// Special Cases:
+// Since lure has org change it's accuracy, the damage is accuracy for lure
+//  so treat lure DAMAGE as lure ACCURACY
+type Gag struct {
+	GagType   string `json:"GagType"`
+	GagName   string `json:"GagName"`
+	Damage    int    `json:"Damage"`
+	OrgDamage int    `json:"OrgDamage"`
+	Accuracy  int    `json:"Accuracy"`
+	Stun      int    `json:"Stun"`
+	Shorthand string `json:"Shorthand"`
+}
+
+// Dict for holding gags
+type GagDictionary map[string]Gag
